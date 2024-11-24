98625a06709a7504f83135a6c5d872c8442ea1c0
atacanbt <157822752+atacanbt@users.noreply.github.com>
2024-11-24 16:11:42 +0000

    week 6 is finished, 7 is halfway through

---
+++ weather.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# above command is called shebang and it tells the system to use bash to run the script
+
+echo "Downloading weather data from Met Eireann API"
+# This script will download the weather data from the Met Eireann API and save it to a file with the current date and time as the filename
+wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
+echo "Finished downloading weather data"
