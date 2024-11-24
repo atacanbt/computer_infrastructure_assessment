b43d3b2111f627254057d0b6fd98c76100b7a314
atacanbt <157822752+atacanbt@users.noreply.github.com>
2024-11-24 17:04:29 +0000

    weather.sh adjusted

--- weather.sh
+++ weather.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 # above command is called shebang and it tells the system to use bash to run the script
 
+date
 echo "Downloading weather data from Met Eireann API"
 # This script will download the weather data from the Met Eireann API and save it to a file with the current date and time as the filename
 wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
 echo "Finished downloading weather data"
+date
